493c62827a66cd0c9cb5acd28d947c3f8239443c
tongfeiguo <tongfei.gf@gmail.com>
2022-04-01 15:49:08 -0400

    a8 dislike count

--- full-stack-developer-server-node/controllers/tuits-controller.js
+++ full-stack-developer-server-node/controllers/tuits-controller.js
@@ -7,6 +7,7 @@ const createTuit = (req, res) => {
     const newTuit = req.body;
     newTuit._id = (new Date()).getTime() + '';
     newTuit.likes = 0;
+    newTuit.dislikes = 0;
     tuits.push(newTuit);
     res.json(newTuit);
 }
