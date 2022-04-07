dc6bceded6cb32a434c8cbbe31b43c4549c1d671
tongfeiguo <tongfei.gf@gmail.com>
2022-04-07 15:22:59 -0400

    a9

--- full-stack-developer-server-node/server.js
+++ full-stack-developer-server-node/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import helloController from "./controllers/hello-controller.js";
-import userController   from "./controllers/user-controller.js";
+import userController from "./controllers/user-controller.js";
 import tuitController from "./controllers/tuits-controller.js";
 import mongoose from "mongoose";
 //a9
-mongoose.connect('mongodb://localhost:27017/webdev');
-
+// mongoose.connect('mongodb://localhost:27017/webdev');
+//a9 moongodb atlas
+mongoose.connect(`mongodb+srv://feliciagtf:shotwell@cluster0.immtk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&authSource=admin`);
 const app = express();
 app.use(cors());
 
